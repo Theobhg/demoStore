9e038d2a114da8b4cf43b714deb196295e6ed43a
Theo Bhering <tbhering@unicadtec.com.br>
2021-01-27 13:20:22 -0300

    retirado comentarios

--- src/components/Product/BeardProduct.js
+++ src/components/Product/BeardProduct.js
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { FavColor } from './styles';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardMedia from '@material-ui/core/CardMedia';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import IconButton from '@material-ui/core/IconButton';
 import { formatPrice } from '../../utils/formatPrice';
@@ -20,16 +19,6 @@ const useStyles = makeStyles((theme) => ({
     height: 0,
     paddingTop: '56.25%', // 16:9
   },
-  // expand: {
-  //   transform: 'rotate(0deg)',
-  //   marginLeft: 'auto',
-  //   transition: theme.transitions.create('transform', {
-  //     duration: theme.transitions.duration.shortest,
-  //   }),
-  // },
-  // expandOpen: {
-  //   transform: 'rotate(180deg)',
-  // },
 }));
 
 function BeardProduct() {
