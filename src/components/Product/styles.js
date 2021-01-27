53ebecf62bbd5d264284a6a8f302c7c72c5e8d11
Theo Bhering <tbhering@unicadtec.com.br>
2021-01-27 15:06:14 -0300

    troca na logica de favoritados

--- src/components/Product/styles.js
+++ src/components/Product/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 
 export const FavColor = styled.button`
-  color: ${({ toggle }) => toggle ? '#c3c3c3' : '#d32f2f'};
+  color: ${({ toggle }) => !toggle ? '#c3c3c3' : '#d32f2f'};
   height: 60px;
   width: 60;
   border-style: none;
